a30f2ef7c368571b7bf6c803e6efc3d62b55268b
Johannes <jowettinger@gmail.com>
2016-07-30 18:54:19 +0200

    smoketest enhanced

--- test/smoketest-cli.sh
+++ test/smoketest-cli.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export EXPECTED=$(cat test/csar-1/expected/definitions.tosca)
+
 bin/toscafy generate \
   --debug \
   -o test-out \
@@ -8,5 +10,10 @@ bin/toscafy generate \
   --clear-output
 
 cat test-out/Definitions/definitions.tosca
+echo " "
+
+export ACTUAL=$(cat test-out/Definitions/definitions.tosca)
 
 rm -rf test-out
+
+node -e "if (process.env.EXPECTED.replace(/\s/g, '') !== process.env.ACTUAL.replace(/\s/g, '')) { console.log('TEST FAILED: generated definitions.tosca file not as expected'); process.exit(1); } else { console.log('TEST PASSED') }"
